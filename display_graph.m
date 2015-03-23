c57268fa22a86f88c859e2508c7e60af5963f0da
jalayrac <jeanbaptiste.alayrac@gmail.com>
2015-03-23 18:11:07 +0100

    Import code

--- display_graph.m
+++ display_graph.m
@@ -1,4 +1,18 @@
-vertices_first  = coords(:, edges(1,:)+1);
-vertices_second = coords(:, edges(2,:)+1);
+vertices_first  = coords(:, edges(1,:)+1)';
+vertices_second = coords(:, edges(2,:)+1)';
+orientations    = edges(3, :);
 
 figure();
+scatter(coords(2,:),coords(1,:),'.','y')
+hold on;
+
+unoriented = orientations==2;
+oriented   = ~unoriented;
+
+% Plot the unoriented in red
+plot([vertices_first(unoriented,2) vertices_second(unoriented,2)]',[vertices_first(unoriented,1) vertices_second(unoriented,1)]','r');
+% Plot oriented in blue
+plot([vertices_first(oriented,2) vertices_second(oriented,2)]',[vertices_first(oriented,1) vertices_second(oriented,1)]','b');
+
+axis equal
+hold off
